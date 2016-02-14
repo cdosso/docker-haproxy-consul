aed6eee63d6fdcbe8edc491e7e417e87b7291881
Carlo Dosso <carlo.dosso@gmail.com>
2016-02-14 16:05:53 +0100

    [WIP] zero downtime works only inside container.

--- scripts/init/haproxy_reload.sh
+++ scripts/init/haproxy_reload.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-exec 1> /proc/1/fd/1
-exec 2> /proc/1/fd/2
-
-exec nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --buffer
-exec sv restart haproxy
-exec nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --release-indefinite
+nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --buffer
+sv restart haproxy
+nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --release-indefinite
